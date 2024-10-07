087df050ffa4dc27c5ee23df6e76944fe6f3df0a
Tadeo1504 <tadeo1504r@.com>
2024-10-06 22:50:03 -0300

    agregado de login

---
+++ src/components/LoginPage.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import "./loginPage.css";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import LockIcon from '@mui/icons-material/Lock';
+
+const preventDefault = (event) => event.preventDefault();
+
+export const LoginPage = () => {
+    return (
+    <div>
+      <h1 className='title-app'>UCUGRAM</h1>
+      <div className='log-box'>
+            <h3 className='log-text'>Iniciar Sesión</h3>
+            <form>
+              <div className="username">
+                <input type="text" placeholder="Usuario" />
+                {/* use AccountCircleIcon on input */}
+                <AccountCircleIcon className='user-icon'></AccountCircleIcon>
+              </div>
+              <div className="password">
+                <input type="password" placeholder="Contraseña" />
+                <LockIcon className='lock-icon'></LockIcon>
+              </div>
+              <button type="submit">Ingresar</button>
+              <Box
+              sx={{
+                display: 'flex',
+                flexWrap: 'wrap',
+                justifyContent: 'center',
+                typography: 'body1',
+                '& > :not(style) ~ :not(style)': {
+                  ml: 2,
+                },
+              }}
+              onClick={preventDefault}
+            >
+              <Link href="#" underline="hover">
+                {'¿Olvidaste tu contraseña?'}
+              </Link>
+            </Box>
+            <Box
+              sx={{
+                display: 'flex',
+                flexWrap: 'wrap',
+                justifyContent: 'center',
+                typography: 'body1',
+                '& > :not(style) ~ :not(style)': {
+                  ml: 2,
+                },
+              }}
+              onClick={preventDefault}
+            >
+              <Link href="#" underline="hover">
+                {'Registrarse'}
+              </Link>
+            </Box>
+            </form>
+          </div>
+    </div> 
+    
+
+    // separacion login y registro
+
+  );
+}
+
+
+
+
+    
